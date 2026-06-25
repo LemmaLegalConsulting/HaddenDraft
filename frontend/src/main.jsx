635bdc87a7d55735fd50d4a6699660d6e551d120
Quinten Steenhuis <qsteenhuis@gmail.com>
2026-06-25 13:42:03 -0400

    Load component bridge before Buckeye theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App.jsx";
 import "./styles/app.css";
+import "./styles/component-bridge.css";
 import "./styles/bux.css";
 
 createRoot(document.getElementById("root")).render(
