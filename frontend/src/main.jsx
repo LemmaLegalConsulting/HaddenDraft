ff3653f70f148f6359c3616d023b7a7ba5e84aec
Quinten Steenhuis <qsteenhuis@gmail.com>
2026-06-25 15:00:36 -0400

    Import local Bootstrap SCSS build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./App.jsx";
+import "./styles/bootstrap-bux.scss";
 import "./styles/app.css";
 import "./styles/bux.css";
 import "./styles/bux-component-theme.css";
